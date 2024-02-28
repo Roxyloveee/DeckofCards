061bddd35433a9a66ed40f57ab4c85d2ea30d934
username <useremail>
2024-02-27 17:51:01 -0800

    done

--- DeckofCards.py
+++ DeckofCards.py
@@ -1,6 +1,6 @@
 import random
 
-class Card
+class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
@@ -22,4 +22,33 @@ class Deck:
             return None
         return self.deck.pop()
     
-    def
+    def count(self):
+        return len(self.deck)
+    
+print("Welcome to the card dealer")
+print("There is a deck of 52 cards.")
+
+deck = Deck()
+
+deck.shuffle()
+
+num_cards_to_deal = int(input("How many cards would you like?" ))
+
+dealt_cards = []
+
+for _ in range(num_cards_to_deal):
+    card = deck.deal()
+    if card is not None:
+        dealt_cards.append(f"{card.rank} of {card.suit}")
+    else:
+        print("No more cards in the deck")
+if dealt_cards:
+    print("Dealt cards: ")
+    for card in dealt_cards:
+        print(card)
+        
+remaining_count = deck.count()
+print(f"Remaining cards in the deck: {remaining_count}")
+
+print("Good luck")
+
